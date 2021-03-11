60beb3b889a338d0e772529289576c6e5c36426c
Imsuong <suongnguyenytm2001@gmail.com>
2021-03-11 20:11:00 +0700

    modified session timeout and cookie-timeout

--- src/suongnguyen/tocotoco/controller/ProductController.java
+++ src/suongnguyen/tocotoco/controller/ProductController.java
@@ -1,15 +1,14 @@
 package suongnguyen.tocotoco.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import suongnguyen.tocotoco.dao.CategoryDAO;
 import suongnguyen.tocotoco.dao.ProductDAO;
-import suongnguyen.tocotoco.model.Category;
 import suongnguyen.tocotoco.model.Product;
 
 public class ProductController extends HttpServlet {
