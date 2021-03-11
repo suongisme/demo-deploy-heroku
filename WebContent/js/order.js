2e26d61c54139664adf6510755efe5236148a694
Imsuong <suongnguyenytm2001@gmail.com>
2021-03-11 19:48:07 +0700

    modified link ajax

--- WebContent/js/order.js
+++ WebContent/js/order.js
@@ -21,7 +21,7 @@ function addEvent() {
 			const id = allBtnAdd[i].id;
 			allAmount[i].textContent = parseInt(allAmount[i].textContent) + 1;
 
-			xml.open('GET', `${protocol}//${host}/clone-tocotoco/cart/add?id=${id}`, true);
+			xml.open('GET', `${protocol}//${host}/cart/add?id=${id}`, true);
 			xml.send();
 		})
 
@@ -38,7 +38,7 @@ function addEvent() {
 				cart.innerHTML = `<p class='items__status'>Bạn chưa chọn sản phẩm nào!</p>`
 			}
 
-			xml.open('GET', `${protocol}//${host}/clone-tocotoco/cart/subtract?id=${id}`, true);
+			xml.open('GET', `${protocol}//${host}/cart/subtract?id=${id}`, true);
 			xml.send();
 		})
 	}
